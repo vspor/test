37457de0b664c3e5baca41d548038e0ced3f34db
Вячеслав Спорышев <vsporyshev@ctc-nn.local>
2017-04-20 14:49:47 +0300

    Добавьте файлы проекта.

---
+++ App1/MainPage.xaml.vb
@@ -0,0 +1,13 @@
+﻿' Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x419
+
+''' <summary>
+''' Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+''' </summary>
+Public NotInheritable Class MainPage
+    Inherits Page
+
+    Private Sub AddUserList_Click(sender As Object, e As RoutedEventArgs) Handles AddUserList.Click
+        Dim list1 As ListView
+
+    End Sub
+End Class
